14a2322d39ede1ca7665fcb7c76de8d8ff43debd
GlauberCastro <glaubercasttro@gmail.com>
2020-07-15 19:09:04 -0300

    aprendendo a pegar varios elementos e colocar em um array

--- JavaScript/Controller/index.js
+++ JavaScript/Controller/index.js
@@ -1,4 +1,4 @@
-var nome = document.querySelector("#exampleInputName");
+/*var nome = document.querySelector("#exampleInputName");
 var sexo = document.querySelectorAll("#form-user-create [name=gender]:Checked");//como o formulario tem o radio, é necessario utilizar o parametro checked para pegar o radio selecionado
 var aniversario  = document.querySelector("#exampleinputBirth");
 var pais = document.querySelector("#exampleinputCountry");
@@ -6,3 +6,25 @@ var email = document.querySelector("#exampleinputEmai");
 var password = document.querySelector("#exampleinputPassword");
 var foto = document.querySelector("#exampleinputFile");
 var admin = document.querySelector("#exempleInputAdmin");
+*/
+
+var fields = document.querySelectorAll("#form-user-create [name]");
+var user ={};
+
+//pegando todos os dados do formulario com foreach
+fields.forEach(function (field, index) {
+  if (field.name == "gender" && field.checked == true) {
+    //como o genero tem 22 campos é necessario ver qual esta checado
+    //console.log(field.id, "sim");
+
+    user[field.name] = field.value;
+
+
+  }else{
+
+    user[field.name] = field.value;
+  }
+ 
+});
+
+console.log(user)
