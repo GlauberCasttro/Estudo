86992c5c00f7da219d5b90728422ae21cb4a262f
GlauberCastro <glaubercasttro@gmail.com>
2020-07-14 21:38:54 -0300

    Atualizando estudos javascript

--- JavaScript/Controller/index.js
+++ JavaScript/Controller/index.js
@@ -1,9 +1,8 @@
 var nome = document.querySelector("#exampleInputName");
-var sexo = document.querySelector("#exampleinputGenderF");
-var sexo = document.querySelector("#exampleinputGenderM");
+var sexo = document.querySelectorAll("#form-user-create [name=gender]:Checked");//como o formulario tem o radio, é necessario utilizar o parametro checked para pegar o radio selecionado
 var aniversario  = document.querySelector("#exampleinputBirth");
 var pais = document.querySelector("#exampleinputCountry");
-var email = document.querySelector("#exampleinputEmail");
-var password = document.querySelector("#exampleinputPassword1");
+var email = document.querySelector("#exampleinputEmai");
+var password = document.querySelector("#exampleinputPassword");
 var foto = document.querySelector("#exampleinputFile");
-var admin = document.querySelector("#");
+var admin = document.querySelector("#exempleInputAdmin");
