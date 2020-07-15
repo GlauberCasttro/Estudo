3f4f42c6b6d489210653eb71808cc0901a916c20
GlauberCastro <glaubercasttro@gmail.com>
2020-07-15 11:31:59 -0300

    criando uma view pelo scafold do visual studio e aplicando o conceito de partialview e reaproveitamento de codigo

--- C#-AspNetCore/Formularios/Formularios/Startup.cs
+++ C#-AspNetCore/Formularios/Formularios/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Formularios.Data;
 
 namespace Formularios
 {
@@ -24,6 +26,9 @@ namespace Formularios
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<FormulariosContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("FormulariosContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
