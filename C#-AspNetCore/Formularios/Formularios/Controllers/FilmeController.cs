a92537b6c7710437ba5723cbd71c5ff408d7403c
GlauberCastro <glaubercasttro@gmail.com>
2020-07-15 10:46:05 -0300

    comecando a trabalhar com formularios

---
+++ C#-AspNetCore/Formularios/Formularios/Controllers/FilmeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Formularios.Controllers
+{
+    public class FilmeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Adicionar()
+        {
+            return View();
+        }
+    }
+}
