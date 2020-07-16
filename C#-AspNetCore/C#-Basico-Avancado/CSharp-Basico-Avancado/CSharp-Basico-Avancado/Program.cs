ba51f02df25966774db2e862c8750753d49b182a
GlauberCastro <glaubercasttro@gmail.com>
2020-07-16 14:38:50 -0300

    ajustando nome do projeto para identificar o que foi estudado

--- C#-AspNetCore/C#-Basico-Avancado/CSharp-Basico-Avancado/CSharp-Basico-Avancado/Program.cs
+++ C#-AspNetCore/C#-Basico-Avancado/CSharp-Basico-Avancado/CSharp-Basico-Avancado/Program.cs
@@ -10,6 +10,30 @@ namespace CSharp_Basico_Avancado
     {
         public static void Main(string[] args)
         {
+            double SumNumbers(List<double[]> setsOfNumbers, int indexOfSetToSum)
+            {
+                //trabalhando com valor null setsOfNumbers?[indexOfSetToSum]? e retorno NaN
+                return setsOfNumbers?[indexOfSetToSum]?.Sum() ?? double.NaN;
+            }
+
+            var sum1 = SumNumbers(null, 0);
+            Console.WriteLine(sum1);  // output: NaN
+
+
+            //lista de array de double
+            var numberSets = new List<double[]>
+                    {
+                        new[] { 1.0, 2.0, 3.0 },
+                        new [] {2.0, 3.0 }
+                    };
+
+            var sum2 = SumNumbers(numberSets, 0);
+            Console.WriteLine(sum2);  // output: 6
+
+            var sum3 = SumNumbers(numberSets, 1);
+            Console.WriteLine(sum3);  // output: NaN
+            Console.ReadKey();
+
         }
     }
 }
